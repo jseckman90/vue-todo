320c218f84a72367beba76cbf70b20c928875980
Josh Seckman <jseckman90@gmail.com>
2021-01-16 21:29:39 -0500

    add todo compontnet

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
-  <div id="app"><Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" /></div>
+  <div id="app">
+    <Header />
+    <AddTodo />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+
 export default {
   name: "App",
-  components: { Todos },
+  components: { Header, Todos, AddTodo },
   data() {
     return {
       todos: [
-        { id: 1, title: "Todo One", completed: false },
-        { id: 2, title: "Todo Two", completed: false },
-        { id: 3, title: "Todo Three", completed: false },
+        { id: 1, title: "Learn Vue.js", completed: false },
+        { id: 2, title: "Learn MORE Vue", completed: false },
+        {
+          id: 3,
+          title: "Use Vue to build something cooler than a todo list",
+          completed: false,
+        },
       ],
     };
   },
@@ -34,5 +45,18 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
